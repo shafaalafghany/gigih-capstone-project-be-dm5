5b4625c1dd8d9a466b932cee84061bc96a54ebbb
Shafa Alafghany <alafghanyshafa@gmail.com>
2022-07-07 02:22:25 +0700

    add: function for verify jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    def authorize_jwt
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        begin
+            @decoded = JsonWebToken.decode(header)
+            @current_user = User.find(@decoded[:id])
+        rescue ActiveRecord::NotFound => e
+            render json: { status: fail, message: e.message }, status: :unauthorized
+        rescue JWT::DecodeError => e
+            render json: { status: fail, message: e.message }, status: :unauthorized
+        end
+    end
 end
